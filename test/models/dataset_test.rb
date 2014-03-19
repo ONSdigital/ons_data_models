acb401b03738a037af424b6733fff8b12961ec79
Mazz Mosley <marianne.mosley@gmail.com>
2014-03-19 14:44:06 +0000

    Skeleton tests for dataset

---
+++ test/models/dataset_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+require "models/dataset"
+
+class DatasetTest < ActiveSupport::TestCase
+  context "with a dataset" do
+
+    should "have a unique slug" do
+      dataset_1 = FactoryGirl.create(:dataset)
+      dataset_2 = FactoryGirl.build(:dataset)
+      assert dataset_1.valid?
+      assert dataset_2.valid? == false
+    end
+
+    should "have a structure hash" do
+      dataset_1 = FactoryGirl.create(:dataset)
+      assert dataset_1.structure.is_a?Hash
+    end
+  end
+end
